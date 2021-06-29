0c185ebe86d54517901c6a7ece209b50ce66075d
Alex Norton <atn362@gmail.com>
2021-06-29 11:59:25 -0500

    added header

---
+++ react_portfolio/src/components/common/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Navbar } from '../../common';
+
+import './Header.css';
+
+function Header () {
+
+    return (
+        <section className="header">
+            <section className="header-logo">
+                    Alexander Todd Norton
+                    
+                    </section>
+                    <div className="navbar">
+                    <Navbar/>
+                    </div>
+                    <div className="links">
+                        <a href="/">About Me</a>
+                        <a href="/">Portfolio</a>
+                        <a href="/">Contacts</a>
+                        <a href="/">Resume</a>
+                    </div>
+            
+    </section>
+            
+    
+        
+    )
+}
+
+export default Header;
---
+++ react_portfolio/src/components/common/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Navbar.css';
+
+function Navbar () {
+    
+    return(
+        <section>
+            Navbar
+        </section>
+    )
+
+}
+
+export default Navbar;
